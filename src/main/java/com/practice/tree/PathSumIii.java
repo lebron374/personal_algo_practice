5d92ae0e2c07250e5c137ea43ef5859fba0b4129
zhi.wang <zhi.wang@beibei.com>
2019-06-14 19:54:25 +0800

    提交代码

---
+++ src/main/java/com/practice/tree/PathSumIii.java
@@ -0,0 +1,30 @@
+package com.practice.tree;
+
+/**
+ * Created by zhi.wang on 2019/6/14.
+ * https://leetcode-cn.com/problems/path-sum-iii/comments/
+ */
+public class PathSumIii {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int pathSum(TreeNode root, int sum) {
+
+        // 以根节点，根左子节点，根右子节点 作为起始节点的数量之和
+        return getPathSum(root, sum) + getPathSum(root.left, sum) + getPathSum(root.right, sum);
+    }
+
+    public int getPathSum(TreeNode root, int sum) {
+        if (null == root) {
+            return 0;
+        }
+
+        return (root.val == sum ? 1:0) + getPathSum(root.left, sum - root.val) + getPathSum(root.right, sum - root.val);
+    }
+
+}
